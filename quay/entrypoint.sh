507675a4cf40d5cfab8f2000e82ab6f4bf518b9a
survivalizer <51409646+survivalizer@users.noreply.github.com>
2020-06-19 22:34:57 -0500

    Add files via upload

---
+++ quay/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Update Server
+if [ ! -z ${SRCDS_APPID} ]; then
+  ./steam/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
